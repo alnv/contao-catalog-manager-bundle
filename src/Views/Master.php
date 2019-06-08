340e353d1c928054fc991aaa90a674cf8db94bcf
Alexander Naumov <me@alexandernaumov.de>
2019-06-08 18:34:24 +0200

    impl master view

--- src/Views/Master.php
+++ src/Views/Master.php
@@ -2,8 +2,23 @@
 
 namespace Alnv\ContaoCatalogManagerBundle\Views;
 
+use Alnv\ContaoCatalogManagerBundle\Helper\ModelWizard;
 
-class Master {
 
-    //
+class Master extends View {
+
+
+    public function parse() {
+
+        $objModel = new ModelWizard( $this->strTable );
+        $objModel = $objModel->getModel();
+        $objEntity = $objModel->findByIdOrAlias( $this->arrOptions['alias'], $this->arrOptions );
+
+        if ( $objEntity !== null ) {
+
+            return $this->parseEntity( $objEntity->row() );
+        }
+
+        return [];
+    }
 }
