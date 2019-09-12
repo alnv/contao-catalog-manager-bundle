4a6bcfe2cfef4a760059a31c2433e608bc6cf224
Alexander Naumov <me@alexandernaumov.de>
2019-09-12 13:25:10 +0200

    add catalog labels

---
+++ src/Resources/contao/languages/de/tl_catalog.php
@@ -0,0 +1,28 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_catalog']['type_settings'] = 'Typ-Einstellungen';
+$GLOBALS['TL_LANG']['tl_catalog']['catalog_settings'] = 'Katalog-Einstellungen';
+$GLOBALS['TL_LANG']['tl_catalog']['mode_settings'] = 'Darstellung-Einstellungen';
+$GLOBALS['TL_LANG']['tl_catalog']['navigation_settings'] = 'Navigation-Einstellungen';
+$GLOBALS['TL_LANG']['tl_catalog']['general_settings'] = 'Allgemeine-Einstellungen';
+$GLOBALS['TL_LANG']['tl_catalog']['geocoding_settings'] = 'Geo-Koordinaten-Einstellungen';
+
+$GLOBALS['TL_LANG']['tl_catalog']['type'] = [ 'Typ', '' ];
+$GLOBALS['TL_LANG']['tl_catalog']['name'] = [ 'Name', 'Bitte geben Sie den Katalognamen ein.' ];
+$GLOBALS['TL_LANG']['tl_catalog']['description'] = [ 'Beschreibung', 'Hier können Sie optional eine Beschreibung zu Ihrem Katalog eingeben.' ];
+$GLOBALS['TL_LANG']['tl_catalog']['table'] = [ 'Tabelle', 'Bitte geben Sie Ihre Wunschbezeichnung für den Tabellen Namen ein.' ];
+$GLOBALS['TL_LANG']['tl_catalog']['enableContentElements'] = [ 'Inhaltselemente verwenden', '' ];
+$GLOBALS['TL_LANG']['tl_catalog']['mode'] = [ 'Darstellung', 'Bitte wählen Sie eine Darstellung der Tabelle im Backend aus.' ];
+$GLOBALS['TL_LANG']['tl_catalog']['showColumns'] = [ 'Als mehrspaltige Tabelle anzeigen', '' ];
+$GLOBALS['TL_LANG']['tl_catalog']['columns'] = [ 'Spalten', '' ];
+$GLOBALS['TL_LANG']['tl_catalog']['flag'] = [ 'Sortierung', '' ];
+$GLOBALS['TL_LANG']['tl_catalog']['enableCopy'] = [ 'Duplizieren erlauben', '' ];
+$GLOBALS['TL_LANG']['tl_catalog']['enableVisibility'] = [ 'Sichtbarkeitssteuerung erlauben', '' ];
+$GLOBALS['TL_LANG']['tl_catalog']['navigation'] = [ 'Navigation', '' ];
+$GLOBALS['TL_LANG']['tl_catalog']['position'] = [ 'Position', '' ];
+$GLOBALS['TL_LANG']['tl_catalog']['enableGeocoding'] = [ 'Geo-Koordinaten aktivieren', '' ];
+$GLOBALS['TL_LANG']['tl_catalog']['geoCity'] = [ 'Stadt-Eingabefeld', '' ];
+$GLOBALS['TL_LANG']['tl_catalog']['geoZip'] = [ 'Postleitzahl-Eingabefeld', '' ];
+$GLOBALS['TL_LANG']['tl_catalog']['geoStreet'] = [ 'Straße-Eingabefeld', '' ];
+$GLOBALS['TL_LANG']['tl_catalog']['geoStreetNumber'] = [ 'Straßennummer-Eingabefeld', '' ];
+$GLOBALS['TL_LANG']['tl_catalog']['geoCountry'] = [ 'Land-Eingabefeld', '' ];
