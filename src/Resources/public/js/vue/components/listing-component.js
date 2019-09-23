92dbb3a1c3ee77a2a4fb9dfa9be3ee8ea872532f
Alexander Naumov <me@alexandernaumov.de>
2019-09-23 14:37:43 +0200

    impl sortable

--- src/Resources/public/js/vue/components/listing-component.js
+++ src/Resources/public/js/vue/components/listing-component.js
@@ -1,8 +1,10 @@
 const listingComponent = Vue.component( 'listing', {
     data: function () {
         return {
-            template: '',
-            parameters: {}
+            view: '',
+            parameters: {
+                order: {}
+            }
         }
     },
     methods: {
@@ -11,14 +13,55 @@ const listingComponent = Vue.component( 'listing', {
                 params: this.parameters
             }).then(function ( objResponse ) {
                 if ( objResponse.body ) {
-                    this.template = objResponse.body.template;
+                    this.view = objResponse.body.template;
                 }
             });
         },
         onChange: function ( shared ) {
             this.parameters = shared;
             this.fetch();
+        },
+        sortable: function () {
+            var objSortable = this.$refs.view.querySelector('.sortable');
+            if ( !objSortable ) {
+                return null;
+            }
+            var arrSortingFields = objSortable.querySelectorAll('[data-sort]');
+            if ( !arrSortingFields.length ) {
+                return null;
+            }
+            var self = this;
+            for ( var i = 0; i < arrSortingFields.length; i++ ) {
+                var objSortField = arrSortingFields[i];
+                var strFieldname = objSortField.dataset.sort;
+                objSortField.dataset.order = this.parameters['order'][ strFieldname ] ? this.parameters['order'][ strFieldname ]['order'] : objSortField.dataset.order;
+                objSortField.classList.remove( 'desc' );
+                objSortField.classList.remove( 'asc' );
+                if ( objSortField.dataset.order ) {
+                    objSortField.classList.add( objSortField.dataset.order );
+                }
+                objSortField.addEventListener('click', function ( objEvent ) {
+                    objEvent.preventDefault();
+                    if ( !this.dataset.sort ) {
+                        return null;
+                    }
+                    this.dataset.order = this.dataset.order === 'asc' || !this.dataset.order ? 'desc' : 'asc';
+                    self.parameters.order[ this.dataset.sort ] = {
+                        'field': this.dataset.sort,
+                        'order': this.dataset.order
+                    };
+                    this.classList.remove( 'desc' );
+                    this.classList.remove( 'asc' );
+                    this.classList.add( this.dataset.order );
+                    self.fetch();
+                });
+            }
         }
+    },
+    updated: function () {
+        this.$nextTick(function () {
+            this.sortable();
+        })
     },
     mounted: function () {
         this.fetch();
@@ -36,8 +79,7 @@ const listingComponent = Vue.component( 'listing', {
         }
     },
     template:
-    '<div class="listing-component">' +
-        '<div class="listing-component-container" v-html="template"></div>' +
+    '<div class="listing-component" ref="view">' +
+        '<div class="listing-component-container" v-html="view"></div>' +
     '</div>'
 });
-
