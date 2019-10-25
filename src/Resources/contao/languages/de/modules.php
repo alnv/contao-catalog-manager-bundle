9d4728a824754155a1f20e11a1779f65ef77ac6d
Alexander Naumov <me@alexandernaumov.de>
2019-10-25 11:46:09 +0200

    change label

--- src/Resources/contao/languages/de/modules.php
+++ src/Resources/contao/languages/de/modules.php
@@ -2,7 +2,7 @@
 
 $GLOBALS['TL_LANG']['MOD']['catalog-manager-bundle'] = 'Catalog Manager';
 $GLOBALS['TL_LANG']['MOD']['catalog-manager'] = [ 'Catalog Manager', '' ];
-$GLOBALS['TL_LANG']['MOD']['catalog-option'] = [ 'Options', '' ];
+$GLOBALS['TL_LANG']['MOD']['catalog-option'] = [ 'Optionen', '' ];
 
 $GLOBALS['TL_LANG']['FMD']['catalog-manager-bundle'] = [ 'Catalog Manager 2.0', 'Alle Frontend Module des Catalog Managers.' ];
 $GLOBALS['TL_LANG']['FMD']['listing'] = [ 'Listenansicht', '' ];
