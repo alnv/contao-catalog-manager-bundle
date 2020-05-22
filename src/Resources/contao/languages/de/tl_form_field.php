0ac32236d5da954b1fc3601cfb8b1ff25fbb0505
Alexander Naumov <mail@alexandernaumov.de>
2020-05-22 12:36:51 +0200

    rm form fields to form manager

--- src/Resources/contao/languages/de/tl_form_field.php
+++ src/Resources/contao/languages/de/tl_form_field.php
@@ -1,9 +1,3 @@
 <?php
 
-$GLOBALS['TL_LANG']['tl_form_field']['optionsSource'] = ['Optionen-Quelle', ''];
-$GLOBALS['TL_LANG']['tl_form_field']['includeBlankOption'] = [' Leere Option hinzufügen', ''];
-$GLOBALS['TL_LANG']['tl_form_field']['blankOptionLabel'] = ['Bezeichnung der leeren Option', ''];
-$GLOBALS['TL_LANG']['tl_form_field']['reference']['optionsSource'] = [
-    'options' => 'Auswahlliste',
-    'dbOptions' => 'Datenbank Tabelle'
-];
+//
