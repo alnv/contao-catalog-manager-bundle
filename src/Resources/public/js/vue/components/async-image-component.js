773bfd12810749dec611bc0dcab6727a7122d024
Alexander Naumov <mail@alexandernaumov.de>
2020-08-28 13:13:37 +0200

    fix js error

--- src/Resources/public/js/vue/components/async-image-component.js
+++ src/Resources/public/js/vue/components/async-image-component.js
@@ -51,7 +51,7 @@ const AsyncImageComponent = Vue.component( 'async-image', {
     },
     updated: function () {
         this.$nextTick(function () {
-            if (!this.sticky) {
+            if (this.sticky) {
                 this.sticky.update();
             }
         })
