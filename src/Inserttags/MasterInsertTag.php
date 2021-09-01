793e5fd12279f8ddaf96977b3856c052eeca9127
Alexander Naumov <mail@alexandernaumov.de>
2021-09-01 15:28:25 +0200

    fix inserttag

--- src/Inserttags/MasterInsertTag.php
+++ src/Inserttags/MasterInsertTag.php
@@ -10,17 +10,16 @@ class MasterInsertTag {
 
         if (is_array($arrFragments) && $arrFragments[0] == 'MASTER' && isset($arrFragments[1])) {
 
-            if (empty($GLOBALS['CM_MASTER'])) {
-                return '';
-            }
-
             $strDefault = $arrFragments[2] ?: '';
-            $varValue = $GLOBALS['CM_MASTER'][$arrFragments[1]];
 
-            if ($varValue == null) {
+            if (empty($GLOBALS['CM_MASTER'])) {
                 return $strDefault;
             }
 
+            $varValue = $GLOBALS['CM_MASTER'][$arrFragments[1]];
+            if ($varValue == null) {
+                return $strDefault;
+            }
             if (is_array($varValue)) {
                 if ($strValue = \Alnv\ContaoCatalogManagerBundle\Helper\Toolkit::parse($varValue)) {
                     return $strValue;
