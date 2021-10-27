a5473ae6cc39f148a5f68880d24d235c3a1fbd4d
Alexander Naumov <mail@alexandernaumov.de>
2021-10-27 17:13:33 +0200

    impl child getter

--- src/Inserttags/MasterInsertTag.php
+++ src/Inserttags/MasterInsertTag.php
@@ -16,24 +16,31 @@ class MasterInsertTag {
                 return $strDefault;
             }
 
+            $varValue = '';
             $strFieldname = explode('>', \StringUtil::decodeEntities(($arrFragments[1]?:'')));
 
             switch ($strFieldname[0]) {
                 case 'getParent':
-                        $varValue = $GLOBALS['CM_MASTER']['getParent']()[$strFieldname[1]];
-                        break;
+                    $varValue = $GLOBALS['CM_MASTER']['getParent']()[$strFieldname[1]];
+                    break;
+                case 'getRelated':
                 case 'getArray':
-                        if (is_array($GLOBALS['CM_MASTER'][$strFieldname[1]]) && isset($strFieldname[2])) {
-                            $strValue = '';
-                            foreach ($GLOBALS['CM_MASTER'][$strFieldname[1]] as $arrEntity) {
-                                $strValue .= $arrEntity[$strFieldname[2]] ?: '';
-                            }
-                            $varValue = $strValue;
+                    if ($strFieldname[0] == 'getRelated') {
+                        $arrArray = $GLOBALS['CM_MASTER']['getRelated']($strFieldname[1]);
+                    } else {
+                        $arrArray = $GLOBALS['CM_MASTER'][$strFieldname[1]];
+                    }
+                    if (is_array($arrArray) && isset($strFieldname[2])) {
+                        $strValue = '';
+                        foreach ($arrArray as $arrEntity) {
+                            $strValue .= $arrEntity[$strFieldname[2]] ?: '';
                         }
-                        break;
-                    default;
-                        $varValue = $GLOBALS['CM_MASTER'][$strFieldname[0]];
-                        break;
+                        $varValue = $strValue;
+                    }
+                    break;
+                default;
+                    $varValue = $GLOBALS['CM_MASTER'][$strFieldname[0]];
+                    break;
             }
 
             if ($varValue == null) {
