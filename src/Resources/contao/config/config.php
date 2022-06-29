30c29bb3eeb5a5ea21271b198298a7cfc4c9ad21
Alexander Naumov <mail@alexandernaumov.de>
2022-06-29 09:49:43 +0200

    fix google tpl

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,6 +1,6 @@
 <?php
 
-define("CATALOG_MANAGER_BUNDLE_VERSION", "2.0.2");
+define("CATALOG_MANAGER_BUNDLE_VERSION", "2.0.3");
 
 array_insert( $GLOBALS['BE_MOD'], 2, [
     'catalog-manager-bundle' => [
