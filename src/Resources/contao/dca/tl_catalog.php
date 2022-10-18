e26c9d854e27d8c5ed2ceb0e7f3e1ea883069fe2
Daniel Jahnsmüller <mail@tastaturberuf.de>
2022-10-18 13:51:00 +0200

    Update tl_catalog.php

--- src/Resources/contao/dca/tl_catalog.php
+++ src/Resources/contao/dca/tl_catalog.php
@@ -56,7 +56,7 @@ $GLOBALS['TL_DCA']['tl_catalog'] = [
             'delete' => [
                 'href' => 'act=delete',
                 'icon' => 'delete.svg',
-                'attributes' => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"'
+                'attributes' => 'onclick="if(!confirm(\'' . ($GLOBALS['TL_LANG']['MSC']['deleteConfirm']??'') . '\'))return false;Backend.getScrollOffset()"'
             ],
             'show' => [
                 'href' => 'act=show',
