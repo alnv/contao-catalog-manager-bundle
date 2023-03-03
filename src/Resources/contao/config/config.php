416175700ab3ed557330fb6e85ce51d47e715dd4
Alexander Naumov <alexandernaumov@Alexander-Naumov-Geschaft.local>
2023-03-03 09:25:48 +0100

    allow more chars for alias

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,6 +1,6 @@
 <?php
 
-define("CATALOG_MANAGER_BUNDLE_VERSION", "2.1.8");
+define("CATALOG_MANAGER_BUNDLE_VERSION", "2.1.9");
 
 array_insert( $GLOBALS['BE_MOD'], 2, [
     'catalog-manager-bundle' => [
