818be0bc1d8905910d62d9ced7fd4f065a1a3dec
Alexander Naumov <alexandernaumov@Alexander-Naumov-Geschaft.local>
2023-04-18 10:51:17 +0200

    change version

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,6 +1,6 @@
 <?php
 
-define("CATALOG_MANAGER_BUNDLE_VERSION", "2.1.12");
+define("CATALOG_MANAGER_BUNDLE_VERSION", "2.1.13");
 
 array_insert( $GLOBALS['BE_MOD'], 2, [
     'catalog-manager-bundle' => [
