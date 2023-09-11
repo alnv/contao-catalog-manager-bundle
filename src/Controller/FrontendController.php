e99d754442a4203f3fd531631072aa5d196e0234
Alexander Naumov <alexandernaumov@Alexander-Naumov-Geschaft.local>
2023-09-11 15:37:55 +0200

    impl pagination

--- src/Controller/FrontendController.php
+++ src/Controller/FrontendController.php
@@ -2,9 +2,9 @@
 
 namespace Alnv\ContaoCatalogManagerBundle\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Alnv\ContaoCatalogManagerBundle\Helper\Toolkit;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
@@ -12,13 +12,15 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
  *
  * @Route("/catalog-manager", defaults={"_scope" = "frontend", "_token_check" = false})
  */
-class FrontendController extends \Contao\CoreBundle\Controller\AbstractController {
+class FrontendController extends \Contao\CoreBundle\Controller\AbstractController
+{
 
     /**
      *
      * @Route("/watchlist/update", methods={"POST"}, name="update-watchlist")
      */
-    public function updateWatchlist() {
+    public function updateWatchlist()
+    {
 
         $this->container->get('contao.framework')->initialize();
 
@@ -30,35 +32,47 @@ class FrontendController extends \Contao\CoreBundle\Controller\AbstractControlle
 
     /**
      *
-     * @Route("/view-listing/{module}/{page}", methods={"POST"}, name="view-listing")
+     * @Route("/view-listing/{module}/{page}", methods={"POST", "GET"}, name="view-listing")
      */
-    public function getViewListing($module, $page) {
+    public function getViewListing($module, $page)
+    {
 
         global $objPage;
+
         $objPage = \PageModel::findByPK($page)->loadDetails();
         $GLOBALS['TL_LANGUAGE'] = $objPage->language;
 
         if (isset($_POST['requestUrl'])) {
-            \Environment::set('request', (\Input::post('requestUrl')?:''));
+            \Environment::set('request', (\Input::post('requestUrl') ?: ''));
         }
 
         (new \Alnv\ContaoCatalogManagerBundle\Hooks\PageLayout())->getMasterByPageId($page, \Input::get('item'));
+
         $objPage->ajaxContext = true;
         $strListing = \Controller::getFrontendModule($module);
         $strListing = \Controller::replaceInsertTags($strListing);
-        return new JsonResponse(['template' => Toolkit::compress($strListing), 'limit' => \Cache::get('limit_' . $module), 'max' => (\Cache::get('max_' . $module) ? true : false)]);
+
+        return new JsonResponse([
+            'template' => Toolkit::compress($strListing),
+            'limit' => \Cache::get('limit_' . $module),
+            'max' => (bool) \Cache::get('max_' . $module)
+        ]);
     }
 
     /**
      *
-     * @Route("/json-listing/{module}/{page}", methods={"POST"}, name="json-listing")
+     * @Route("/json-listing/{module}/{page}", methods={"POST", "GET"}, name="json-listing")
      */
-    public function getJsonListing($module, $page) {
+    public function getJsonListing($module, $page)
+    {
 
         global $objPage;
+
         $objPage = \PageModel::findByPK($page)->loadDetails();
         $GLOBALS['TL_LANGUAGE'] = $objPage->language;
-        (new \Alnv\ContaoCatalogManagerBundle\Hooks\PageLayout())->getMasterByPageId($page,\Input::get('item'));
+
+        (new \Alnv\ContaoCatalogManagerBundle\Hooks\PageLayout())->getMasterByPageId($page, \Input::get('item'));
+
         $objPage->ajaxContext = true;
         $objModule = \ModuleModel::findByPk($module);
 
@@ -74,19 +88,29 @@ class FrontendController extends \Contao\CoreBundle\Controller\AbstractControlle
         $objModule = new $strClass($objModule);
         $objModule->setOptions();
         $arrOptions = $objModule->getOptions();
+
         $arrOptions['id'] = $module;
-        return new JsonResponse(['results'=>(new \Alnv\ContaoCatalogManagerBundle\Views\Listing($objModule->getTable(), $arrOptions))->parse()]);
+
+        $objListing = new \Alnv\ContaoCatalogManagerBundle\Views\Listing($objModule->getTable(), $arrOptions);
+
+        return new JsonResponse([
+            'results' => $objListing->parse(),
+            'pagination' => $objListing->getPagination(),
+            'limit' => \Cache::get('limit_' . $module),
+            'max' => (bool) \Cache::get('max_' . $module)
+        ]);
     }
 
     /**
      *
      * @Route("/view-map/{module}/{page}", methods={"GET"}, name="view-map")
      */
-    public function getViewMap($module, $page) {
+    public function getViewMap($module, $page)
+    {
 
         global $objPage;
         $objPage = \PageModel::findByPK($page)->loadDetails();
-        (new \Alnv\ContaoCatalogManagerBundle\Hooks\PageLayout())->getMasterByPageId($page,\Input::get('item'));
+        (new \Alnv\ContaoCatalogManagerBundle\Hooks\PageLayout())->getMasterByPageId($page, \Input::get('item'));
         $objPage->ajaxContext = true;
         $strListing = \Controller::getFrontendModule($module);
         return new JsonResponse(['locations' => $strListing]);
@@ -96,20 +120,21 @@ class FrontendController extends \Contao\CoreBundle\Controller\AbstractControlle
      *
      * @Route("/async-image", methods={"POST"}, name="async-image")
      */
-    public function getAsyncImage() {
+    public function getAsyncImage()
+    {
 
-        $this->container->get( 'contao.framework' )->initialize();
+        $this->container->get('contao.framework')->initialize();
         $arrReturn = [
             'src' => null,
             'alt' => ''
         ];
-        $objEntity = new \Alnv\ContaoCatalogManagerBundle\Views\Master( \Input::post('table'), [
+        $objEntity = new \Alnv\ContaoCatalogManagerBundle\Views\Master(\Input::post('table'), [
             'alias' => \Input::post('id'),
             'id' => '1'
         ]);
         $objRoleResolver = \Alnv\ContaoCatalogManagerBundle\Library\RoleResolver::getInstance(\Input::post('table'), $objEntity->parse()[0]);
         $arrImage = $objRoleResolver->getValueByRole(\Input::post('role'));
-        if ( is_array( $arrImage ) && !empty( $arrImage ) ) {
+        if (is_array($arrImage) && !empty($arrImage)) {
             $arrReturn['src'] = $arrImage[0]['img']['srcset'];
             $arrReturn['alt'] = $arrImage[0]['alt'];
         }
@@ -120,23 +145,24 @@ class FrontendController extends \Contao\CoreBundle\Controller\AbstractControlle
      *
      * @Route("/icalendar", methods={"GET"}, name="getICalendar")
      */
-    public function getICalendar() {
+    public function getICalendar()
+    {
 
-        $this->container->get( 'contao.framework' )->initialize();
+        $this->container->get('contao.framework')->initialize();
         global $objPage;
-        $objPage = \PageModel::findByPK( \Input::get('p') )->loadDetails();
-        $objEntity = new \Alnv\ContaoCatalogManagerBundle\Views\Master( \Input::get('t'), [
+        $objPage = \PageModel::findByPK(\Input::get('p'))->loadDetails();
+        $objEntity = new \Alnv\ContaoCatalogManagerBundle\Views\Master(\Input::get('t'), [
             'alias' => \Input::get('i'),
             'id' => '1'
         ]);
         $arrMaster = $objEntity->parse()[0] ?: [];
-        $objICalendar = new \Alnv\ContaoCatalogManagerBundle\Library\ICalendar( $arrMaster );
+        $objICalendar = new \Alnv\ContaoCatalogManagerBundle\Library\ICalendar($arrMaster);
         return new Response(
             $objICalendar->getICalFile(),
             200,
             [
                 'Content-Type' => 'text/calendar;charset=utf-8',
-                'Content-Disposition' => 'attachment;filename="'. $arrMaster['roleResolver']()->getValueByRole('alias') .'.ics"'
+                'Content-Disposition' => 'attachment;filename="' . $arrMaster['roleResolver']()->getValueByRole('alias') . '.ics"'
             ]
         );
     }
