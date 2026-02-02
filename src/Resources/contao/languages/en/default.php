69576f051453773309181990875d8a091082b377
fritzmg <fmg@inspiredminds.at>
2026-02-02 17:05:14 +0000

    Fix some English translations

--- src/Resources/contao/languages/en/default.php
+++ src/Resources/contao/languages/en/default.php
@@ -1,34 +1,34 @@
 <?php
 
-$GLOBALS['TL_LANG']['MSC']['optionSourceDbTable'] = ['Tabelle', ''];
-$GLOBALS['TL_LANG']['MSC']['optionSourceDbKey'] = ['Datenbankwert', ''];
-$GLOBALS['TL_LANG']['MSC']['optionSourceDbLabel'] = ['Bezeichnung', ''];
-$GLOBALS['TL_LANG']['MSC']['optionSourceDbFilterType'] = ['Eingabetyp', ''];
-$GLOBALS['TL_LANG']['MSC']['optionSourceDbFilterColumn'] = ['Spalten', ''];
-$GLOBALS['TL_LANG']['MSC']['optionSourceDbFilterValue'] = ['Werte', ''];
-$GLOBALS['TL_LANG']['MSC']['optionSourceDbWizardFilterSettings'] = ['Filtereinstellungen', ''];
-$GLOBALS['TL_LANG']['MSC']['optionSourceDbOrderField'] = ['Sortierungsfeld', ''];
-$GLOBALS['TL_LANG']['MSC']['optionSourceDbOrder'] = ['Reihenfolge', ''];
+$GLOBALS['TL_LANG']['MSC']['optionSourceDbTable'] = ['Table', ''];
+$GLOBALS['TL_LANG']['MSC']['optionSourceDbKey'] = ['Database value', ''];
+$GLOBALS['TL_LANG']['MSC']['optionSourceDbLabel'] = ['Label', ''];
+$GLOBALS['TL_LANG']['MSC']['optionSourceDbFilterType'] = ['Input type', ''];
+$GLOBALS['TL_LANG']['MSC']['optionSourceDbFilterColumn'] = ['Columns', ''];
+$GLOBALS['TL_LANG']['MSC']['optionSourceDbFilterValue'] = ['Values', ''];
+$GLOBALS['TL_LANG']['MSC']['optionSourceDbWizardFilterSettings'] = ['Filter settings', ''];
+$GLOBALS['TL_LANG']['MSC']['optionSourceDbOrderField'] = ['Sort field', ''];
+$GLOBALS['TL_LANG']['MSC']['optionSourceDbOrder'] = ['Sorting', ''];
 
 $GLOBALS['TL_LANG']['MSC']['reference']['optionSourceDbFilterType'] = [
-    'expert' => 'Experte',
-    'wizard' => 'Wizard'
+    'expert' => 'Expert',
+    'wizard' => 'Wizard (classic)'
 ];
 
 $GLOBALS['TL_LANG']['MSC']['reference']['optionSourceDbOrder'] = [
-    'asc' => 'Aufsteigend',
-    'desc' => 'Absteigend'
+    'asc' => 'Ascending',
+    'desc' => 'Descending'
 ];
 
-$GLOBALS['TL_LANG']['DCA']['preview'] = ['Vorschau', 'In der Vorschau öffnen.'];
-$GLOBALS['TL_LANG']['MSC']['tstamp'] = ['Änderungsdatum', 'Datum und Uhrzeit der letzten Änderung.'];
-$GLOBALS['TL_LANG']['MSC']['start'] = ['Anzeigen ab', 'Den Artikel erst ab diesem Datum anzeigen.'];
-$GLOBALS['TL_LANG']['MSC']['stop'] = ['Anzeigen bis', 'Den Artikel nur bis zu diesem Datum anzeigen.'];
-$GLOBALS['TL_LANG']['MSC']['published'] = ['Artikel veröffentlichen', 'Den Artikel auf der Webseite anzeigen.'];
-$GLOBALS['TL_LANG']['MSC']['alias'] = ['Artikelalias', 'Der Artikelalias ist eine eindeutige Referenz, die anstelle der numerischen Artikel-ID aufgerufen werden kann.'];
+$GLOBALS['TL_LANG']['DCA']['preview'] = ['Preview', 'Open in preview.'];
+$GLOBALS['TL_LANG']['MSC']['tstamp'] = ['Revision date', 'Date and time of the latest revision.'];
+$GLOBALS['TL_LANG']['MSC']['start'] = ['Show from', 'If you want to prevent the article from showing on the website before a certain date/time, you can enter it here. Otherwise, leave the field blank.'];
+$GLOBALS['TL_LANG']['MSC']['stop'] = ['Show until', 'If you want to prevent the article from showing on the website after a certain date/time, you can enter it here. Otherwise, leave the field blank.'];
+$GLOBALS['TL_LANG']['MSC']['published'] = ['Publish article', 'Make the article publicly visible on the website.'];
+$GLOBALS['TL_LANG']['MSC']['alias'] = ['Article alias', 'The article alias is a unique reference to the article which can be called instead of its numeric ID.'];
 
 $GLOBALS['TL_LANG']['MSC']['watchListTitleLabel'] = 'Name';
-$GLOBALS['TL_LANG']['MSC']['watchListUnitLabel'] = 'Menge';
-$GLOBALS['TL_LANG']['MSC']['watchListAddButtonLabel'] = 'Zur Wunschliste hinzufügen';
-$GLOBALS['TL_LANG']['MSC']['watchListRemoveButtonLabel'] = 'Aus der Wunschliste entfernen';
-$GLOBALS['TL_LANG']['MSC']['watchListUnitsButtonLabel'] = 'Anzahl ändern';
+$GLOBALS['TL_LANG']['MSC']['watchListUnitLabel'] = 'Quantity';
+$GLOBALS['TL_LANG']['MSC']['watchListAddButtonLabel'] = 'Add to wishlist';
+$GLOBALS['TL_LANG']['MSC']['watchListRemoveButtonLabel'] = 'Remove from wishlist';
+$GLOBALS['TL_LANG']['MSC']['watchListUnitsButtonLabel'] = 'Change quantity';
--- src/Resources/contao/languages/fr/modules.php
+++ src/Resources/contao/languages/fr/modules.php
@@ -6,6 +6,7 @@ $GLOBALS['TL_LANG']['MOD']['watchlist'] = ['Liste de souhaits', ''];
 $GLOBALS['TL_LANG']['MOD']['catalog-option'] = ['Options', ''];
 $GLOBALS['TL_LANG']['MOD']['catalog-element'] = ['Éléments de contenu', ''];
 $GLOBALS['TL_LANG']['MOD']['reactions'] = ['Réactions', ''];
+$GLOBALS['TL_LANG']['MOD']['roles'] = ['Rôles', ''];
 $GLOBALS['TL_LANG']['CTE']['component'] = ['Composant Vue', ''];
 $GLOBALS['TL_LANG']['CTE']['listview'] = ['Vue liste', ''];
 $GLOBALS['TL_LANG']['FMD']['catalog-manager-bundle'] = ['Catalog Manager', ''];
